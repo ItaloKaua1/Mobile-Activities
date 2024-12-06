d410facfe081e9e49706426138aa54b3ee619d20
ItaloKaua1 <italo.kaua.11@gmail.com>
2024-12-06 17:57:35 -0300

    ultimas mudancas
    adc todos os planetas do sistema solar

--- PlanetApp/app/src/main/java/com/example/planetapp/models/Planet.kt
+++ PlanetApp/app/src/main/java/com/example/planetapp/models/Planet.kt
@@ -17,6 +17,26 @@ data class Planet(
 val planetList = listOf(
     Planet(
         id = 1,
+        name = "Mercury",
+        type = "Terrestrial",
+        galaxy = "Milky Way",
+        distanceFromSun = "57.9 million km",
+        diameter = "4,880 km",
+        characteristics = "Closest planet to the Sun, no atmosphere, extreme temperatures.",
+        imageRes = R.drawable.mercurio
+    ),
+    Planet(
+        id = 2,
+        name = "Venus",
+        type = "Terrestrial",
+        galaxy = "Milky Way",
+        distanceFromSun = "108.2 million km",
+        diameter = "12,104 km",
+        characteristics = "Thick toxic atmosphere, hottest planet, similar in size to Earth.",
+        imageRes = R.drawable.venus
+    ),
+    Planet(
+        id = 3,
         name = "Earth",
         type = "Terrestrial",
         galaxy = "Milky Way",
@@ -25,4 +45,54 @@ val planetList = listOf(
         characteristics = "Supports life, has water and atmosphere.",
         imageRes = R.drawable.terra
     ),
+    Planet(
+        id = 4,
+        name = "Mars",
+        type = "Terrestrial",
+        galaxy = "Milky Way",
+        distanceFromSun = "227.9 million km",
+        diameter = "6,779 km",
+        characteristics = "Known as the Red Planet, has the largest volcano and canyon.",
+        imageRes = R.drawable.marte
+    ),
+    Planet(
+        id = 5,
+        name = "Jupiter",
+        type = "Gas Giant",
+        galaxy = "Milky Way",
+        distanceFromSun = "778.5 million km",
+        diameter = "139,820 km",
+        characteristics = "Largest planet, has a Great Red Spot, strong magnetic field.",
+        imageRes = R.drawable.jupiter
+    ),
+    Planet(
+        id = 6,
+        name = "Saturn",
+        type = "Gas Giant",
+        galaxy = "Milky Way",
+        distanceFromSun = "1.4 billion km",
+        diameter = "116,460 km",
+        characteristics = "Famous for its prominent ring system.",
+        imageRes = R.drawable.saturno
+    ),
+    Planet(
+        id = 7,
+        name = "Uranus",
+        type = "Ice Giant",
+        galaxy = "Milky Way",
+        distanceFromSun = "2.87 billion km",
+        diameter = "50,724 km",
+        characteristics = "Rotates on its side, has a pale blue color due to methane.",
+        imageRes = R.drawable.urano
+    ),
+    Planet(
+        id = 8,
+        name = "Neptune",
+        type = "Ice Giant",
+        galaxy = "Milky Way",
+        distanceFromSun = "4.5 billion km",
+        diameter = "49,244 km",
+        characteristics = "Deep blue color, has the fastest winds in the Solar System.",
+        imageRes = R.drawable.netuno
+    )
 )
