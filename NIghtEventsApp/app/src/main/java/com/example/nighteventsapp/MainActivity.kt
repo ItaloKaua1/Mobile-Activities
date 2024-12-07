03981003a002cb2853858d05b3f1c30c7fe49c4f
ItaloKaua1 <italo.kaua.11@gmail.com>
2024-12-07 14:09:39 -0300

    mudancas parciais

--- NIghtEventsApp/app/src/main/java/com/example/nighteventsapp/MainActivity.kt
+++ NIghtEventsApp/app/src/main/java/com/example/nighteventsapp/MainActivity.kt
@@ -3,45 +3,69 @@ package com.example.nighteventsapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.nighteventsapp.ui.theme.NIghtEventsAppTheme
+import androidx.compose.ui.platform.LocalContext
+import androidx.navigation.compose.*
+import com.example.nighteventsapp.ui.components.BottomNavigationBar
+import com.example.nighteventsapp.ui.components.DrawerContent
+import com.example.nighteventsapp.ui.components.TopBar
+import com.example.nighteventsapp.ui.screens.*
+import com.example.nighteventsapp.ui.theme.NightEventsAppTheme
+import kotlinx.coroutines.launch
 
+@ExperimentalMaterial3Api
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            NIghtEventsAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+            val navController = rememberNavController()
+            val drawerState = rememberDrawerState(DrawerValue.Closed)
+            val scope = rememberCoroutineScope()
+            val isDarkTheme = remember { mutableStateOf(false) }
+
+            NightEventsAppTheme(darkTheme = isDarkTheme.value) {
+                ModalNavigationDrawer(
+                    drawerState = drawerState,
+                    gesturesEnabled = true,
+                    drawerContent = {
+                        DrawerContent(navController) {}
+                    },
+                    content = {
+                        Scaffold(
+                            topBar = {
+                                TopBar(
+                                    onThemeToggle = { isDarkTheme.value = !isDarkTheme.value },
+                                    onOpenDrawer = { scope.launch { drawerState.open() } }
+                                )
+                            },
+                            bottomBar = { BottomNavigationBar(navController) }
+                        ) { innerPadding ->
+                            NavHost(
+                                navController = navController,
+                                startDestination = "home",
+                                modifier = Modifier.padding(innerPadding)
+                            ) {
+                                composable("home") {
+                                    HomeScreen(navController, context = LocalContext.current)
+                                }
+                                composable("events") {
+                                    SubscribedEventsScreen(navController)
+                                }
+                                composable("favorites") {
+                                    FavoriteScreen(navController)
+                                }
+                                composable("eventDetails/{eventId}") { backStackEntry ->
+                                    val eventId = backStackEntry.arguments?.getString("eventId")
+                                    EventDetailsScreen(eventId = eventId)
+                                }
+                            }
+                        }
+                    }
+                )
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NIghtEventsAppTheme {
-        Greeting("Android")
     }
 }
--- NIghtEventsApp/app/src/main/java/com/example/nighteventsapp/ui/components/TopBar.kt
+++ NIghtEventsApp/app/src/main/java/com/example/nighteventsapp/ui/components/TopBar.kt
@@ -1,2 +1,26 @@
 package com.example.nighteventsapp.ui.components
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.BrightnessHigh
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+
+@ExperimentalMaterial3Api
+@Composable
+fun TopBar(
+    onThemeToggle: () -> Unit,
+    onOpenDrawer: () -> Unit
+) {
+    TopAppBar(
+        title = {Text("EventApp")},
+        navigationIcon = {
+            IconButton(onClick = onOpenDrawer) {
+                Icon(Icons.Default.BrightnessHigh, contentDescription = "Toggle Theme")
+            }
+        }
+    )
+}
