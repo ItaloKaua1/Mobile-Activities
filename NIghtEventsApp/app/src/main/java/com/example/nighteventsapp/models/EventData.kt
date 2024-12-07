5c8bfdd3e716036f312fe34a3738d4ef2cc7e55d
ItaloKaua1 <italo.kaua.11@gmail.com>
2024-12-07 16:49:12 -0300

    ultimos ajustes

--- NIghtEventsApp/app/src/main/java/com/example/nighteventsapp/models/EventData.kt
+++ NIghtEventsApp/app/src/main/java/com/example/nighteventsapp/models/EventData.kt
@@ -26,4 +26,45 @@ val eventList = listOf(
         isSubscribed = mutableStateOf(false),
         imageRes = R.drawable.img1
     ),
+    Event(
+        id = 2,
+        title = "Workshop de Inteligência Artificial",
+        description = "Aprenda a criar modelos de IA do zero.",
+        date = "2024-12-20",
+        location = "Centro de Inovação",
+        isFavorite = mutableStateOf(false),
+        isSubscribed = mutableStateOf(false),
+        imageRes = R.drawable.img2
+    ),
+    Event(
+        id = 3,
+        title = "Feira de Startups 2024",
+        description = "Conectando empreendedores e investidores.",
+        date = "2025-01-10",
+        location = "Espaço Startup Hub",
+        isFavorite = mutableStateOf(false),
+        isSubscribed = mutableStateOf(false),
+        imageRes = R.drawable.img3
+    ),
+    Event(
+        id = 4,
+        title = "Hackathon Internacional",
+        description = "48 horas de programação para criar soluções inovadoras.",
+        date = "2025-02-01",
+        location = "Campus Universitário",
+        isFavorite = mutableStateOf(false),
+        isSubscribed = mutableStateOf(false),
+        imageRes = R.drawable.img4
+    ),
+    Event(
+        id = 5,
+        title = "Palestra sobre Sustentabilidade Tecnológica",
+        description = "A tecnologia como aliada do meio ambiente.",
+        date = "2025-03-05",
+        location = "Auditório Verde",
+        isFavorite = mutableStateOf(false),
+        isSubscribed = mutableStateOf(false),
+        imageRes = R.drawable.img5
+    )
+
 )
